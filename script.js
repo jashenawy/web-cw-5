d50fbfd6fa982a9189ea0a62b28891e059300072
jashenawy <100783932+jashenawy@users.noreply.github.com>
2022-03-16 19:27:40 +0300

    done

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 alert("jana")
-console.log("%c 16","color: red");
+console.log("%c 16","color:red");
 console.log("%c انا في الصف العاشر","color: red");
 //جافا سكريبت ليست جافا 
+
+
+
+let budget= 20;
+console.log(budget > 30 && budget < 80);
+
+budget += 30;
+console.log(budget > 30 && budget < 80 );
