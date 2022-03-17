043b7604113fb2a70ac372aad2609ab299be8d4a
jashenawy <100783932+jashenawy@users.noreply.github.com>
2022-03-17 15:49:11 +0300

    done

--- script.js
+++ script.js
@@ -10,3 +10,56 @@ console.log(budget > 30 && budget < 80);
 
 budget += 30;
 console.log(budget > 30 && budget < 80 );
+
+
+// let grade = prompt("what is your grade?");
+
+// if(grade >= 90) {
+// console.log("🥳قد حصلت على امتياز");
+// } else  if (grade >= 80) {
+// console.log("لقد حصلت على جيد جدا");
+// } else if( grade>= 70) {
+//     console.log("لقد حصل على جيد");
+// } else if(grade >= 60) {
+//     console.log("قد حصلت على مقبول");
+// } else  if (grade >= 50) {
+//     console.log("قد حصلت على ضعيف");
+// } else  {
+//     console.log("راسب");
+// }
+
+
+let =prompt("ماهي نتيجتك");
+
+ var grade ="60";
+
+switch(grade){
+    case"90":
+    alert("قد حصلت على امتياز");
+    break;
+    
+
+    case"80":
+    alert("قد حصلت على جيد جدا");
+    break;
+
+    case" 70":
+    alert("قد حصلت على جيد");
+    break;
+    default:  alert("حدث خطأ");
+    
+}
+
+
+// let grade = prompt("ما هي نتيجتك");
+// switch(grade){
+//     case(grade >= 90):
+//     console.log("ممتاز");
+//     break;
+// }
+
+
+
+
+
+
